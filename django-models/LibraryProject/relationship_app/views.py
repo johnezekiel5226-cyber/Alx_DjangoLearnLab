8b1097fe64acca91eb7f3942d09f7f1cc07cdcd1
Ezekiel John <johnezekiel526@gmail.com>
2025-11-05 04:47:48 -0800

    Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 from .models import Library
-
+from django.views.generic.detail import DetailView
 # --- Function-based view using template ---
 def book_list(request):
     """
