960092bcebcd31d084b691abcb08f6b7ce279ede
Ezekiel John <johnezekiel526@gmail.com>
2025-11-06 05:55:43 -0800

    Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -62,35 +62,16 @@ def is_librarian(user):
 def is_member(user):
     return hasattr(user, 'profile') and user.profile.role == 'Member'
 
-@permission_required('yourapp.can_add_book', raise_exception=True)
+@permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')
-    else:
-        form = BookForm()
-    return render(request, 'add_book.html', {'form': form})
-
-@permission_required('yourapp.can_change_book', raise_exception=True)
+    ...
+@permission_required('relationship_app.can_change_book', raise_exception=True)
 def edit_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    form = BookForm(request.POST or None, instance=book)
-    
-    if form.is_valid():
-        form.save()
-        return redirect('book_list')
-
-    return render(request, 'edit_book.html', {'form': form})
-
-@permission_required('yourapp.can_delete_book', raise_exception=True)
+    ...
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
 def delete_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.delete()
-        return redirect('book_list')
-    return render(request, 'confirm_delete.html', {'book': book})
+    ...
+
 
 # -------------------------------
 # Role-Based Dashboard Views
