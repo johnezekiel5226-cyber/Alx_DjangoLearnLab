b966b3d814f17498f20c7f1ae68aff8bc8d51d5a
Ezekiel John <johnezekiel526@gmail.com>
2025-11-14 02:16:35 -0800

    bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser  # import your custom user model
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'is_staff', 'is_active']
+
+admin.site.register(CustomUser, CustomUserAdmin)
