1305af6636668b41016b27bd63dc1a7b8c55dbd8
Ezekiel John <johnezekiel526@gmail.com>
2025-11-14 05:37:50 -0800

    CustomUser Admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -5,5 +5,10 @@ from .models import CustomUser  # import your custom user model
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ['username', 'email', 'is_staff', 'is_active']
+    list_filter = ("is_staff", "is_active")
+    fieldsets = UserAdmin.fieldsets
+    add_fieldsets = UserAdmin.add_fieldsets
+    search_fields = ("email", "username")
+    ordering = ("email",)
 
 admin.site.register(CustomUser, CustomUserAdmin)
