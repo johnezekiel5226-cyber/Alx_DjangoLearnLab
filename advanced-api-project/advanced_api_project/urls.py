581abda53ffb5fc3b34fc7d97ceecc1b5daef0fd
Ezekiel John <johnezekiel526@gmail.com>
2025-11-25 06:31:34 -0800

    api

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import test_api
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', test_api),
 ]
