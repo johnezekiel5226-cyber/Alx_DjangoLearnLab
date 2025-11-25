b2c5822e4a12997725d6c4c6db6ae487509d1bc4
Ezekiel John <johnezekiel526@gmail.com>
2025-11-25 06:39:39 -0800

    api

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -11,6 +11,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    publication_year = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
