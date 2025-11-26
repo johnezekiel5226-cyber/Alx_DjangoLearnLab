012888117af2211fb52ed7df663dad3f9ff83489
Ezekiel John <johnezekiel526@gmail.com>
2025-11-26 05:21:47 -0800

    advanced api 2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 # api/views.py
 from rest_framework import generics
 from rest_framework.filters import SearchFilter
+from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import generics, permissions
