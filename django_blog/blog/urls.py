9ee9c094b03b5ccbec1a5fcf2cfa7aa7a40128a0
Ezekiel John <johnezekiel526@gmail.com>
2025-12-03 03:35:19 -0800

    user Authentication

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,13 +9,16 @@ PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 app_name = 'posts'
 urlpatterns = [
 path('posts/', PostListView.as_view(), name='post-list'),
-path('posts/new/', PostCreateView.as_view(), name='post-create'),
 path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
 path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 path("register/", views.register, name="register"),
 path("profile/", views.profile, name="profile"),
 path("profile/edit/", views.edit_profile, name="edit_profile"),
+path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+]
 ]
 
 urlpatterns = [
