3e92ec3087b933a998d0f18ea32f89653b5afe5f
Ezekiel John <johnezekiel526@gmail.com>
2025-12-03 06:42:35 -0800

    comments

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -21,6 +21,9 @@ path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), na
 path('<int:post_id>/comments/new/', views.add_comment, name='comment_new'),
 path('<int:post_id>/comments/<int:pk>/edit/', views.CommentEditView.as_view(), name='comment_edit'),
 path('<int:post_id>/comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
+path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_new'),
+path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
+path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
 
 
