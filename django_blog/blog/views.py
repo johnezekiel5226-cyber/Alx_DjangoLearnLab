fe865c9ef11b7ef204542f0feb041ffeb5fcdefc
Ezekiel John <johnezekiel526@gmail.com>
2025-12-03 06:38:39 -0800

    comments

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -48,6 +48,32 @@ class CommentEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         # only the author can edit
         comment = self.get_object()
         return comment.author == self.request.user
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def form_valid(self, form):
+        # attach post + author
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        form.instance.post = post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
+
+    def test_func(self):
+        comment = self.get_object()
+        return comment.author == self.request.user
 
 class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
