259cbca8a343ac86360779c0425f950adbe2f169
Ezekiel John <johnezekiel526@gmail.com>
2025-12-09 01:58:45 -0800

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,11 +1,24 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
-from .models import User
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'following']
+        fields = [
+            'id', 
+            'username', 
+            'email', 
+            'bio', 
+            'profile_picture', 
+            'followers',
+            'following'
+        ]
+        read_only_fields = ['followers', 'following']
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -15,19 +28,31 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
+        # Create user using get_user_model()
         user = User.objects.create_user(
-            username = validated_data['username'],
-            email = validated_data.get('email'),
-            password = validated_data['password']
+            username=validated_data.get('username'),
+            email=validated_data.get('email'),
+            password=validated_data.get('password'),
         )
+
+        # Create authentication token
+        Token.objects.create(user=user)
+
         return user
+
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
-    password = serializers.CharField()
+    password = serializers.CharField(write_only=True)
 
-    def validate(self, data):
-        user = authenticate(**data)
-        if user and user.is_active:
-            return user
-        raise serializers.ValidationError("Invalid login credentials")
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            raise serializers.ValidationError("Invalid username or password")
+
+        attrs['user'] = user
+        return attrs
