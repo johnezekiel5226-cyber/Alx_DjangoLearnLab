28c47c7232ad1968503d43aeaa30b30bca33201a
Ezekiel John <johnezekiel526@gmail.com>
2025-12-09 03:44:06 -0800

    Implementing Posts and Comments Functionality

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Post, Comment
+
+User = get_user_model()
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'author',
+            'title',
+            'content',
+            'created_at',
+            'updated_at'
+        ]
+        read_only_fields = ['id', 'author', 'created_at', 'updated_at']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.Read
